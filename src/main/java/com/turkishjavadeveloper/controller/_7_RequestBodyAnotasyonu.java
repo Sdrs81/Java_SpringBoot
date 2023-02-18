ee3014524ac8b97048903acde603f43249bc1c34
Sdrs81 <serdardemirtas25@gmail.com>
2023-02-18 16:43:12 +0300

    RequestBody annotation has been added

--- src/main/java/com/turkishjavadeveloper/controller/_7_RequestBodyAnotasyonu.java
+++ src/main/java/com/turkishjavadeveloper/controller/_7_RequestBodyAnotasyonu.java
@@ -1,11 +1,32 @@
 package com.turkishjavadeveloper.controller;
 
+import com.turkishjavadeveloper.model.User;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
-@RequestMapping(path = "/api")
+//@RequestMapping(path = "/api")
 public class _7_RequestBodyAnotasyonu {
 
+    // Bu özelliği veri olarak nesne vermek istediğimizde kullanıyoruz
+    // Gelen json verisini nesneye dönüştürüyor
+
+    @PostMapping("users")  // / koymasak da çalışır, Spring otomatik ekliyor
+    public User saveUser(@RequestBody User user){
+        System.out.println("User saved");
+        user.setPassword("");
+        return user;
+    }
+
+    @PostMapping("users-all")  // / koymasak da çalışır, Spring otomatik ekliyor
+    public List<User> saveAllUsers(@RequestBody List<User> users){
+        System.out.println("All users saved");
+        users.forEach(user -> user.setPassword(""));
+        return users;
+    }
 
 }
