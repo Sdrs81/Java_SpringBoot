06d9d4681298c086aa48a25242e315d9b1475b2b
Sdrs81 <serdardemirtas25@gmail.com>
2023-03-03 23:30:22 +0300

    Scope annotatiton has been added

---
+++ src/Old_Lessons/ch09_scope_anotasyonu/SecondController.java
@@ -0,0 +1,19 @@
+package com.turkishjavadeveloper.ch09_scope_anotasyonu;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class SecondController {
+
+    @Autowired
+    private MyComponent myComponent;
+
+    @GetMapping("/second-controller")
+    private String getNameOfComponent(){
+        return this.myComponent.getName();
+    }
+}
